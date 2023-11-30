5e29dc45aab42ba3b7d1ef85cd1dc236496da6c7
yms0606 <ymsix0622@gmail.com>
2023-11-30 17:37:15 +0900

    [minsik] add review listview

--- app/src/main/java/com/example/dbproject/fragment/ReviewFragment.kt
+++ app/src/main/java/com/example/dbproject/fragment/ReviewFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dbproject.R
 import com.example.dbproject.databinding.FragmentReviewBinding
+import com.example.dbproject.databinding.ItemReviewDetailBinding
 
 class ReviewFragment : Fragment() {
 
@@ -21,6 +24,41 @@ class ReviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_review,container,false)
+
+
+
+        binding.reviewRecyclerview.adapter = ReviewRecyclerViewAdapter()
+        binding.reviewRecyclerview.layoutManager = LinearLayoutManager(activity)
         return binding.root
+    }
+
+    inner class ReviewHolder(var binding: ItemReviewDetailBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ReviewRecyclerViewAdapter(): RecyclerView.Adapter<ReviewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewHolder {
+            var view = ItemReviewDetailBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+            return ReviewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 2
+        }
+
+        override fun onBindViewHolder(holder: ReviewHolder, position: Int) {
+            var examplelist = arrayListOf<String>()
+            var examplelistacc = arrayListOf<String>()
+            var viewHolder = holder.binding
+
+            examplelist.add("식당1")
+            examplelist.add("식당2")
+            examplelistacc.add("aaa@naver.com")
+            examplelistacc.add("bbb@naver.com")
+            viewHolder.reviewListResname.text = examplelist[position]
+            viewHolder.reviewListAccname.text = examplelistacc[position]
+
+
+        }
+
+
     }
 }
